4bd37454b1640c5b7169cba1901a486f8bb6c891
ilhan2651 <95929697+ilhan2651@users.noreply.github.com>
2024-05-13 17:26:57 +0300

    Add files via upload

---
+++ AbstractClass/DatabaseTestMethods.cs
@@ -0,0 +1,64 @@
+﻿using AbstractClass;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public abstract class BaseDb
+    {
+        public DataTable ExecuteSql(string sql)
+        {
+            // generate sql
+            return new DataTable();
+        }
+
+        public abstract string GetDbVersion();
+    }
+
+
+
+    public class MySqlDb : BaseDb
+    {
+        public override string GetDbVersion() => "MySql  Server ";
+
+
+
+    }
+
+
+
+    class SqlServerDb : BaseDb
+    {
+        public override string GetDbVersion() => "SqlServer 2012";
+
+
+        public string GenerateSql(int id)
+        {
+            return $"SELECT * FROM USERS WITH(NOLOCK) WHERE ID ={id}";
+
+
+        }
+
+    }
+
+    public class OracleDb : BaseDb
+    {
+        public override string GetDbVersion() => "Oracle 18c";
+
+        public string GenerateSql(int id)
+        {
+            return $"SELECT * FROM USERS WITH(NOLOCK) WHERE USER_ID ={id}";
+
+        }
+
+
+    }
+    
+}
+
+       
+
---
+++ AbstractClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sqlDb = new SqlServerDb();
+            sqlDb.ExecuteSql("");
+
+            var oracleDb = new OracleDb();
+            oracleDb.ExecuteSql("");
+            oracleDb.GetDbVersion();
+
+            
+
+        }
+    }
+}
